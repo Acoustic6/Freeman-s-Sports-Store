66f55615f94f32e22691b4e811eaf6ffa4f5acd7
Acoustic6 <dorozhko.dmitry@gmail.com>
2018-10-20 12:58:56 +0300

    ...

---
+++ SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a5738fae-c004-4847-89e8-32464cb09dcc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public int PageSize = 4;
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            repository = productRepository;
+        }
+
+        public ViewResult List(int page = 1)
+        {
+            return View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));
+        }
+    }
+}
