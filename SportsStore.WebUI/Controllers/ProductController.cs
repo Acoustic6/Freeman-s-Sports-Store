be07f4a9719ee76405380a4cc74cdc279f580d24
Acoustic6 <dorozhko.dmitry@gmail.com>
2018-10-21 12:51:39 +0300

    adding category and customize routing

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -17,11 +17,12 @@ namespace SportsStore.WebUI.Controllers
             repository = productRepository;
         }
 
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         {
             var model = new ProductsListViewModel
             {
                 Products = repository.Products
+                    .Where(p => category == null || p.Category == category)
                     .OrderBy(p => p.ProductID)
                     .Skip((page - 1) * PageSize)
                     .Take(PageSize),
@@ -30,7 +31,8 @@ namespace SportsStore.WebUI.Controllers
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
                     TotalItems = repository.Products.Count()
-                }
+                },
+                CurrentCategory = category
             };
 
             return View(model);
